cc87fb37d8d3654da8cde5a1321447fec647eb7d
hobbes <hobbes@Artemis>
2022-02-24 02:39:23 +0200

    Started develop read of tables-section

--- document/Document.cpp
+++ document/Document.cpp
@@ -34,8 +34,12 @@ Document::Document(string file) {
 	File f(file);
 
     bool sectionStart = false;
+    bool tableHeader = false;
+
     string section;
-    vector<Group> entity;
+
+    vector<Group> entity;		// Buffer of one entitity lines
+    vector<Group> table;
 
     Group g;
     while (f.readGroup(g)) {
@@ -60,20 +64,53 @@ Document::Document(string file) {
     		continue;
     	}
 
-    	// Read entities, later for other sections also
+    	/*
+    	 * Tables section, read known table types. Layer, Ltype, style first
+    	 */
+    	if (section == "TABLES") {
+
+
+    		if (g.groupcode == 0 && g.value == "TABLE") {
+    			// start new table. Put current table (If exists = not nullptr) to tables of Document (Map, key: table type)
+    			// dxf allows many table sections with same type?? need merge if exists
+    			tableHeader = true;
+    		}
+    		else if (g.groupcode == 0 && g.value == "ENDTAB") { // table ready
+    			// create (last) table entry from tables-vector and add to current table object.
+    			// Close table.
+
+    		}
+    		else if (g.groupcode == 2) {	// Start new table entry
+    			if (tableHeader) {
+    				tableHeader = false;
+    				// Finalize table header here. Means create table object from table-vector
+    				// Can but table-object all ready here to tables map. Better ???
+    				// Better here. Otherwise need put both TABLE & ENDTABLE elements, so that also last table of tables section get saved
+    			}
+    			else {
+    				if (table.size() > 0) {
+    					// Create new table entry from table-vector and add to current table object.
+    					// Instead flag can use directly table = null.
+    				}
+    			}
+    			table.clear();		// Clean previous objects data
+    			table.push_back(g); // Type of table entry.
+    		}
+    		else {
+    			table.push_back(g);	// Add to data vector, used to create new object (Table or table entry) when next start or table ends.
+    		}
+    	}
+
+
+    	/*
+    	 * In entities section, read known entities, list unknown to stdout
+    	 */
     	if (section == "ENTITIES") {
 
     		if (g.groupcode == 0) { // start new entity
 
     			// if previous entity > 0, print entity
     			if (entity.size() > 0) {
-    			/*	bool first = true;
-    				cout << "  {";
-    				for (const auto &a : entity) {
-    					cout << (first ? "" : ",") << "\"" << a.groupcode << "\" : \"" << a.value << "\"";
-    					first = false;
-    				}
-    				cout << "}" << endl; */
 
     				Entity* e = ef.create(entity);
     				if (e == nullptr) {
@@ -99,15 +136,11 @@ Document::~Document() {
 	for (unsigned int i = 0; i < entities_.size(); ++i) {
 		Entity* e = entities_[i];
 
+		// Debug code during development. Shows entityes have been created
 		cout << e->toString() << endl;
 
 		delete e;
 	}
-}
-
-void Document::createEntity(const vector<string>& props) {
-
-
 }
 
 
